c5c1ca7d79f0d6d6d806f2f8d2c26766c81a7193
Cherry <duncanlinnaeus98@gmail.com>
2021-03-18 15:15:03 +0300

    initial commit

---
+++ app/src/main/java/com/example/smartcarpark/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.smartcarpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.bookbtn);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intentButton = new Intent(MainActivity.this, Homepage.class);
+                startActivity(intentButton);
+            }
+        });
+        Button signIn = (Button)findViewById(R.id.signin);
+        signIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intentSignIn = new Intent(MainActivity.this, Registration.class);
+                startActivity(intentSignIn);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/smartcarpark/Registration.java
@@ -0,0 +1,25 @@
+package com.example.smartcarpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        Button Register = (Button)findViewById(R.id.regbut);
+        Register.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intentRegister = new Intent(Registration.this, Homepage.class);
+                startActivity(intentRegister);
+            }
+        });
+    }
+}
