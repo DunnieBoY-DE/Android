026cedc224e1d6f36a2b7f428b62b8ebe2d5aa6a
Cherry <duncanlinnaeus98@gmail.com>
2021-03-25 19:35:37 +0300

    Create a side navigation menu

--- app/src/main/java/com/example/smartcarpark/Homepage.java
+++ app/src/main/java/com/example/smartcarpark/Homepage.java
@@ -14,36 +14,36 @@ ActivityHomepageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding =ActivityHomepageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container,new FirstFragment())
-                .commit();
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.home:
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container,new SecondFragment())
-                            .commit();
-                    case R.id.vehicle:
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container,new ThirdFragment())
-                            .commit();
-                    case R.id.profile:
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container,new FourthFragment())
-                            .commit();
-                case R.id.setting:
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container, new FifthFragment())
-                            .commit();
-            }
-            return true;
-        });
+//        binding =ActivityHomepageBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.fragment_container,new FirstFragment())
+//                .commit();
+//        binding.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+//            switch (item.getItemId()){
+//                case R.id.home:
+//                    getSupportFragmentManager()
+//                            .beginTransaction()
+//                            .replace(R.id.fragment_container,new SecondFragment())
+//                            .commit();
+//                    case R.id.vehicle:
+//                    getSupportFragmentManager()
+//                            .beginTransaction()
+//                            .replace(R.id.fragment_container,new ThirdFragment())
+//                            .commit();
+//                    case R.id.profile:
+//                    getSupportFragmentManager()
+//                            .beginTransaction()
+//                            .replace(R.id.fragment_container,new FourthFragment())
+//                            .commit();
+//                case R.id.setting:
+//                    getSupportFragmentManager()
+//                            .beginTransaction()
+//                            .replace(R.id.fragment_container, new FifthFragment())
+//                            .commit();
+//            }
+//            return true;
+//        });
     }
 }
